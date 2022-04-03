df8ffd2e33a582ebd2f7deaff1ae89ea6573df33
Loi Van Tran <datloiboi@gmail.com>
2022-04-04 06:28:14 +0700

    Complete navigation

---
+++ Swift-RandomPhoto/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Swift-RandomPhoto
+//
+//  Created by Loi Van Tran on 4/4/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
