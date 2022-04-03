54add4ea8abf60c2853898ee9e88b913fb749c11
Loi Van Tran <datloiboi@gmail.com>
2022-04-03 13:56:47 +0700

    Initial Commit

---
+++ Swift-RandomPhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-RandomPhoto
+//
+//  Created by Loi Van Tran on 4/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-RandomPhoto/Swift_RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_RandomPhotoApp.swift
+//  Swift-RandomPhoto
+//
+//  Created by Loi Van Tran on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
